0e79e922b615e3baaaf6a981f9f62c14d9d4245c
Alex <ignatenko2207@gmail.com>
2021-01-14 21:59:03 +0200

    Add Navigation Page parsing

--- src/main/java/ua/mainacademy/ApplicationRunner.java
+++ src/main/java/ua/mainacademy/ApplicationRunner.java
@@ -1,17 +1,47 @@
 package ua.mainacademy;
 
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import ua.mainacademy.service.DocumentExtractorService;
-import ua.mainacademy.service.ItemPageParsingService;
+import ua.mainacademy.model.Item;
+import ua.mainacademy.service.RouterService;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ApplicationRunner
 {
     public static void main( String[] args )
     {
-        String url = "https://www.amazon.com/GeForce-i7-10750H-Windows-Keyboard-15-dh1020nr/dp/B087YVJ44P";
+//        String url = "https://www.amazon.com/MSI-A10SFS-287-Professional-i7-10875H-Win10PRO/dp/B08DHY3VF6";
 
-        System.out.println(ItemPageParsingService.getItemFromPage(url));
+        String keyWord = args.length == 0 ? "hp omen 15" : args[0];
+        String url = "https://www.amazon.com";
+        String searchUrl = url + "/s?k=" + keyWord.replaceAll(" ", "+");
 
+        List<Item> items = Collections.synchronizedList(new ArrayList<>());
+        List<Thread> threads = Collections.synchronizedList(new ArrayList<>());
+
+        RouterService routerService = new RouterService(items, threads, searchUrl);
+        threads.add(routerService);
+        routerService.start();
+
+        do {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (!threadsAreNotActive(threads));
+
+        System.out.println("Items were extracted. Amount=" + items.size());
+
+    }
+
+    private static boolean threadsAreNotActive(List<Thread> threads) {
+        for (Thread thread: threads) {
+            if (thread.isAlive() || thread.getState().equals(Thread.State.NEW)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/ua/mainacademy/service/ItemPageParsingService.java
+++ src/main/java/ua/mainacademy/service/ItemPageParsingService.java
@@ -1,36 +1,85 @@
 package ua.mainacademy.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import ua.mainacademy.model.Item;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class ItemPageParsingService {
+@AllArgsConstructor
+public class ItemPageParsingService extends Thread{
 
-    public static Item getItemFromPage(String url) {
-        Document document = DocumentExtractorService.getDocument(url);
-        Element element = document.getElementById("ppd");
+    private List<Item> items;
+    private Document document;
+    private String url;
 
-        String name = extractName(element);
+    public static boolean isItemPage(String url) {
+        return  !url.contains("/dp/");
+    }
+
+    @Override
+    public void run() {
+        items.add(getItemFromPage(url));
+    }
+
+    public Item getItemFromPage(String url) {
+        Element productBlock = document.getElementById("dp-container");
+
+        String name = extractName(productBlock);
         String code = extractCode(url);
-        int price = extractPrice(element);
-        int initPrice = extractInitPrice(element) == 0 ? price : extractInitPrice(element);
-//        String imageUrl = extractImageUrl(element);
-//        String group = extractGroup(document);
-//        String seller = extractSeller(element);
+        int price = extractPrice(productBlock);
+        int initPrice = extractInitPrice(productBlock) == 0 ? price : extractInitPrice(productBlock);
+        String imageUrl = extractImageUrl(productBlock);
+        String group = extractGroup(document);
+        String seller = extractSeller(productBlock);
 
         return Item.builder()
                 .code(code)
                 .name(name)
                 .price(price)
                 .initPrice(initPrice)
-//                .group(group)
+                .group(group)
                 .url(url)
-//                .imageUrl(imageUrl)
-//                .seller(seller)
+                .imageUrl(imageUrl)
+                .seller(seller)
                 .build();
+    }
+
+    private static String extractSeller(Element element) {
+        Element sellerElement = element.getElementById("tabular-buybox-truncate-1").getElementsByClass("tabular-buybox-text").first();
+        return sellerElement.text();
+    }
+
+    private static String extractGroup(Document document) {
+        String result = "";
+        Element groupDiv = document.getElementById("wayfinding-breadcrumbs_feature_div");
+        Elements groupElements = groupDiv.getElementsByTag("a");
+        List<String> groups = new ArrayList<>();
+        for (Element element: groupElements) {
+            groups.add(element.text());
+        }
+        return StringUtils.join(groups, ">");
+    }
+
+    private static String extractImageUrl(Element element) {
+        Element imageElement = element.getElementById("landingImage");
+        String imageLinks = imageElement.attr("data-a-dynamic-image");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Map result = objectMapper.readValue(imageLinks, Map.class);
+            return (String) result.keySet().iterator().next();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return StringUtils.substringBefore(StringUtils.substringAfter(imageLinks, "{\""), "\":");
     }
 
     private static int extractInitPrice(Element element) {
